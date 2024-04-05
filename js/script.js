ed8e39a324deddca46c65a6ef17fd8cf971df92c
Thivina2002 <136422598+thivinakudagama@users.noreply.github.com>
2024-04-05 20:42:23 +0530

    changed the theme button

--- js/script.js
+++ js/script.js
@@ -91,14 +91,14 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 var icon=document.getElementById('sw-icon');
 
 icon.onclick=()=>{
-    if(icon.classList.contains('bxs-sun')){
-        icon.classList.remove('bxs-sun');
-        icon.classList.add('bxs-moon');
+    if(icon.classList.contains('bxs-moon')){
+        icon.classList.remove('bxs-moon');
+        icon.classList.add('bxs-sun');
         document.body.classList.add("light-theme");
         document.body.classList.remove("dark-theme");
     }else{
-        icon.classList.add('bxs-sun');
-        icon.classList.remove('bxs-moon');
+        icon.classList.add('bxs-moon');
+        icon.classList.remove('bxs-sun');
         document.body.classList.add("dark-theme");
         document.body.classList.remove("light-theme");
     }
